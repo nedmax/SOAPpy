a5241978e8ec51f44f762099fe33607a20395267
Nedimar Paulo Turatti <nedimar@corp.globo.com>
2015-09-29 11:27:42 -0300

    Fix setup.

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-#
-# $Id: setup.py,v 1.11 2005/02/15 16:32:22 warnes Exp $
 
 CVS=0
 
@@ -13,11 +11,11 @@ def read(*rnames):
     ).read()
 url="https://github.com/kiorky/SOAPpy.git"
 long_description="SOAPpy provides tools for building SOAP clients and servers.  For more information see " + url\
-    +'\n'+read('README.txt')\
-    +'\n'+read('CHANGES.txt')
+    +'\n'+read('README.md')\
+    +'\n'+read('CHANGES.md')
 setup(
     name="SOAPpy",
-    version='0.12.23.dev0',
+    version='0.12.24',
     description="SOAP Services for Python",
     maintainer="Gregory Warnes, kiorky",
     maintainer_email="Gregory.R.Warnes@Pfizer.com, kiorky@cryptelium.net",
@@ -31,4 +29,3 @@ setup(
         'defusedxml',
     ]
 )
-
